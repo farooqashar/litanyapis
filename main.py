1a290b7636c5302870b1a8149530e1b32b9f9d16
Ashar Farooq <ashar744@gmail.com>
2021-06-24 19:43:47 -0400

    added files and revised requirements

--- main.py
+++ main.py
@@ -282,6 +282,11 @@ def inspirequotes():
 
   return render_template('inspirequotes.html',all_data = all_data)
 
+@app.route('/favicon.ico')
+def favicon():
+
+  return render_template('favicon.html')
+
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
 		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
